a4cfb493c4eb15fafd30d9bbb3ccae0d0d524deb
Aleksey Litvinov <iamhoneyboy@gmail.com>
2023-08-13 21:21:43 +0100

    add css support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document () {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='stylesheet' href='/app.css' />
+        <link rel='stylesheet' href='/tachyons.min.css' />
+      </Head>
       <body>
         <Main />
         <NextScript />
